93c40a473eda83e8622c4dadd14acb205bd5625a
Artem Fomenko <fomenko.artem2005@mail.ru>
2023-09-30 10:40:22 +0300

    Теперь программа спрашивает время и выводит соответствующее сообщение

--- hello.pas
+++ hello.pas
@@ -1,3 +1,13 @@
 ﻿begin
-  Print('Hello,World!');
+  var h := ReadInteger('Введите текущий час:');
+  Assert(h < 24,'В сутках 24 часа! Полночь - это 0 часов');
+  
+  if (h >= 4) and (h <= 10) then
+    Print('Доброе утро, мир!')
+  else if (h >= 11) and (h <= 16) then
+    Print('Добрый день, мир!')
+  else if (h >= 17) and (h <= 22) then
+    Print('Добрый вечер, мир!')
+  else
+    Print('Доброй ночи, мир!');
 end.
