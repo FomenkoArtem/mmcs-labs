c8b64cc534e9c66d7f1cfcd81b23f92653e3b8d7
Artem Fomenko <fomenko.artem2005@mail.ru>
2023-09-30 10:47:42 +0300

    Программа, которая определяет, високосный ли год

---
+++ time.pas
@@ -0,0 +1,9 @@
+﻿begin
+  var year := ReadInteger('Введите год:');
+  Assert(year > 0,'Такого года не существует');
+  
+  if (year mod 4 = 0) and (year mod 100 <> 0) and (year mod 400 <> 0) then
+    Print('Год високосный')
+  else
+    Print('Год не високосный');
+end.
